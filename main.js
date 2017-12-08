653c7f1ff763f42bd0fd3bb591810160bc6cde56
LordBard42 <eberger3@ashland.edu>
2017-12-08 09:37:49 -0500

    Tested DOM THings
    ^

---
+++ main.js
@@ -0,0 +1,46 @@
+
+var cookies = 0;
+var cursors = 0;
+var cursorCost = 0;
+var firstName;
+
+function setName() {                                                          //After someone enters there name, this removes the field and
+  firstName = document.getElementById("fName").value;                         //Adds info
+  document.getElementById("nameFields").innerHTML = "Name: " + firstName;     //https://www.w3schools.com/jsref/dom_obj_style.asp
+  document.getElementById("nameFields").style.fontSize = "150%";
+  document.getElementById("position").innerHTML = "Position: Amateur";
+  document.getElementById("position").style.fontSize = "150%";
+}
+
+/*
+function cookieClick(number) {
+
+  cookies = cookies + number;
+  document.getElementById("cookies").innerHTML = cookies;
+
+}
+
+function buyCursor() {
+  cursorCost = Math.floor(10*Math.pow(1.1,cursors))     //Works Out cost for current cursor
+  if(cookies >= cursorCost){                                //Checks that playerr can buy cursor
+    cursors = cursors + 1;
+    cookies = cookies - cursorCost;
+    document.getElementById("cursors").innerHTML = cursors;
+    document.getElementById("cookies").innerHTML = cookies;
+  }
+  var nextCost = Math.floor(10*Math.pow(1.1,cursors));            //Updates the user of cursor cost
+  document.getElementById("cursorCost").innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+
+	cookieClick(cursors);
+
+  if(cookies < cursorCost){
+
+  }
+
+    //  document.getElementById("buyCursor").disabled = false;
+
+}, 1000);
+*/
