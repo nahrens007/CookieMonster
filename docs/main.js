6a5215d36f9a08633e4ce51f671adbb7bb32dcee
LordBard42 <eberger3@ashland.edu>
2017-12-11 09:46:28 -0500

    Trying to Fix my Version

--- docs/main.js
+++ docs/main.js
@@ -1,19 +1,9 @@
-
 var cookies = 0;
 var cursors = 0;
 var cursorCost = 0;
 var firstName;
 
 function setName() {                                                          //After someone enters there name, this removes the field and
-<<<<<<< HEAD
-  firstName = document.getElementById("fName").value;                         //Adds info
-  document.getElementById("nameFields").innerHTML = "";     //https://www.w3schools.com/jsref/dom_obj_style.asp
-  document.getElementById("nameFields").style.fontSize = "150%";
-  document.getElementById("position").style.fontSize = "150%";
-  document.getElementById("heading").innerHTML = "Cookie Monster &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp " +
-                    "Name: " + firstName +  "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Position: Amateur";
-
-=======
 
   firstName = document.getElementById("fName").value;                         // Retrieves name from user
 
@@ -67,17 +57,13 @@ function loadCookies() {
 /* runs on HTML body onLoad event */
 function bodyOnLoad() {
   loadCookies();
->>>>>>> d8ac18e8027295e050d0111871fa995929fcd22b
 }
 
 /*
 function cookieClick(number) {
-
   cookies = cookies + number;
   document.getElementById("cookies").innerHTML = cookies;
-
 }
-
 function buyCursor() {
   cursorCost = Math.floor(10*Math.pow(1.1,cursors))     //Works Out cost for current cursor
   if(cookies >= cursorCost){                                //Checks that playerr can buy cursor
@@ -89,16 +75,10 @@ function buyCursor() {
   var nextCost = Math.floor(10*Math.pow(1.1,cursors));            //Updates the user of cursor cost
   document.getElementById("cursorCost").innerHTML = nextCost;
 };
-
 window.setInterval(function(){
-
 	cookieClick(cursors);
-
   if(cookies < cursorCost){
-
   }
-
     //  document.getElementById("buyCursor").disabled = false;
-
 }, 1000);
 */
