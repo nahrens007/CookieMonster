357debb591e93a5ee941fbc8e91416277b20bcc6
Nathan Ahrens <nathanmahrens@gmail.com>
2017-12-13 13:41:49 -0500

    Completely reorganized javascript.
    Changed names of functions to be more descriptive/accurate. Added
    comments to function headers to epxlain what they're for. Changed
    organization slightly.

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
-var firstName,position;
-var hideHeading;
+var firstName;
+var position;
 var storyText = "All sounds are muted. The world around is warm and comforting but "+
   "strangely is not as roomy as you first remebered. Suddenly, a light appears and " +
   "you are thrust towards the light. You think to yourself, 'Here goes nothing...'";
@@ -9,39 +9,34 @@ var parentsAdoration = 0;
 var selfWill = 0;
 
 /*
-setNameClick(): function is activated when the "submit" button is clicked on the name input form.
+function: gameLoop():
+1) Runs the game loop. Is called only by initiateGame()!
+ */
+function gameLoop() {
+
+}
+
+/*
+function: initiateGame()
 This function is responsible for:
-1) Retrieving the name from the input box in the name input form
-2) Setting the name and position variables
-3) Setting the name/position cookies
-4) Setting the visibility of the header (cookie and html/css).
+1) Setting the display of any elements
+correctly, as well as any game variables (other than the name and position,
+which have been set by either setNameClick() or loadCookies()).
+2) Starting the ticker tape animation
 */
-function setNameClick() {                                                          //After someone enters there name, this removes the field and
-
-  firstName = document.getElementById("fName").value;
-                        // Retrieves name from user
-
-  // if the firstName input field was empty, set it to a generic name.
-  if (firstName == "" || firstName == null){
-    firstName = "Guest";
-  }
-
-  document.getElementById("game").style.display = "block";
-                     // Set the display div to block so that the user may begin playing
-  document.getElementById("nameInputFields").innerHTML = "";                  //https://www.w3schools.com/jsref/dom_obj_style.asp
-
-  position = "Baby";                                                       // Clients always start as an amateur.
-  hideHeading = "inline";                                                        // The heading should not be hidden since the name and position are set.
-
-  updateHeader();                                                             // Update the heading (name/position)
-
-  //Shows the first buttons
+function initiateGame(){
+  //Shows the first buttons and set starting stats
   document.getElementById("buttons").style.display = "inline";
 
-  // Update the cookies
-  setCookie("name", firstName, 30);
-  setCookie("position", position, 30);
-  setCookie("headingVisibility", hideHeading, 30);
+  document.getElementById("gamePlayer").innerHTML = "Name: " + firstName;
+  document.getElementById("gamePosition").innerHTML = "Position: " + position;
+  document.getElementById("tapeContainer").style.display = "block";
+  document.getElementById("game").style.display = "block";
+  document.getElementById("gamePlayer").style.display = "inline";
+  document.getElementById("gamePosition").style.display = "inline";
+
+  // Start ticker tape
+  animateTape(storyText, 2400);
 }
 
 /*This will determine what happens when giggle is cliked.
@@ -79,101 +74,112 @@ function cryClick(){
   }
 
 }
-/*
-Will update the information in the top header when we need it to.
+
+/* function: bodyOnLoad():
+Runs when the HTML body loads (onload attribute)
 */
-function updateHeader(){
-
-  document.getElementById("gamePlayer").innerHTML = "Name: " + firstName;
-  document.getElementById("gamePosition").innerHTML = "Position: " + position;
-
-  if (hideHeading == "inline")
-  {
-    document.getElementById("gamePlayer").style.display = "inline";
-    document.getElementById("gamePosition").style.display = "inline";
-    document.getElementById("pAdoration").style.display = "inline";
-  } else {
-    document.getElementById("gamePlayer").style.display = "none";
-    document.getElementById("gamePosition").style.display = "none";
-  }
-
-  // Start ticker tape
-  myMove(storyText, 2450);
-}
-/*This is going to have scrolling text to introduce the game like a ticker tape.
-https://www.w3schools.com/js/js_htmldom_animate.asp*/
-function myMove(text, containterSize){
-  var elem = document.getElementById("tapeAnimate");
-  var id = setInterval(frame,15);
-  var box = document.querySelector("#tapeContainer");  //https://www.w3schools.com/jsref/prop_element_offsetwidth.asp
-  var width = box.offsetWidth + 300;
-  var pos = width;
-
-  elem.style.width = containterSize + "px";
-
-  //Starts tape offscreen so you don't see a flash of text on start
-  elem.style.left = pos + "px";
-  elem.innerHTML = text;
-
-  function frame(){
-    if(pos <= -containterSize){
-      //clearInterval(id);
-      pos = box.offsetWidth + 100;
-    }else{
-      pos--;
-      elem.style.left = pos + "px";
-    }
-
-  }
-
-}
-
-/* return the cookie at name cname: https://www.w3schools.com/js/js_cookies.asp */
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-/* Load the cookies */
-function loadCookies() {
-  firstName = getCookie("name");
-  hideHeading = getCookie("headingVisibility");
-  position = getCookie("position");
-
-  // set the name to name from cookies
-  if (firstName != "")
-  {
-      document.getElementById("nameInputFields").innerHTML = "";
-      updateHeader();
-  }else {
-      document.getElementById("nameInputFields").style.display = "block";
-  }
-}
-
-/* runs on HTML body onLoad event */
 function bodyOnLoad() {
 
   loadCookies();
 }
 
+/*
+function: animateTape():
+1) This is going to have scrolling text to introduce the game like a ticker tape.
+https://www.w3schools.com/js/js_htmldom_animate.asp
+*/
+function animateTape(text, containterSize){
+  var animatedElement = document.getElementById("tapeAnimate");
+  var id = setInterval(frame,15);
+  var box = document.querySelector("#tapeContainer");  //https://www.w3schools.com/jsref/prop_element_offsetwidth.asp
+  var pos = box.offsetWidth + 50;
 
-window.setInterval(function(){
-  document.getElementById("pAdorationBar").style.height = parentsAdoration + "px";
-}, 1);
+  animatedElement.style.width = containterSize + "px";
+
+  //Starts tape offscreen so you don't see a flash of text on start
+  animatedElement.style.left = pos + "px";
+  animatedElement.innerHTML = text;
+
+  function frame(){
+    if(pos <= -containterSize){
+      //clearInterval(id);
+      pos = box.offsetWidth + 50;
+    }else{
+      pos-=5;
+      animatedElement.style.left = pos + "px";
+    }
+  }
+}
+
+/*
+setNameClick(): function is activated when the "submit" button is clicked on the name input form.
+This function is responsible for:
+1) Retrieving the name from the input box in the name input form
+2) Setting the name and position variables
+3) Setting the name/position cookies
+4) Setting the visibility of the header (cookie and html/css).
+*/
+function setNameClick() {
+
+  firstName = document.getElementById("fName").value;
+  position = "Baby"; 
+
+  // if the firstName input field was empty, set it to a generic name.
+  if (firstName == "" || firstName == null){
+    firstName = "Guest";
+  }
+
+  /* nameInputFields is no longer needed since the name has been retrieved */
+  document.getElementById("nameInputFields").innerHTML = "";                  //https://www.w3schools.com/jsref/dom_obj_style.asp
+
+  // Set the cookies - should expire after 356 days.
+  setCookie("name", firstName, 356);
+  setCookie("position", position, 356);
+
+  initiateGame(); // Update the heading (name/position) and prepare to start
+}
+
+/*
+function: loadCookies():
+1) Load the cookies for the game
+- if cookies have been set: load cookies and start game by calling initiateGame()
+- else (cookies not set): display nameInputFields to allow user to enter name
+*/
+function loadCookies() {
+  firstName = getCookie("name");
+  position = getCookie("position");
+
+  // set the name to name from cookies
+  if (firstName != "")
+  {
+    document.getElementById("nameInputFields").innerHTML = "";
+    initiateGame();
+  }else {
+    document.getElementById("nameInputFields").style.display = "block";
+  }
+}
+
+/* set the cookie at name cname to value cvalue, to expire in exdays */
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/* return the cookie at name cname: https://www.w3schools.com/js/js_cookies.asp */
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
